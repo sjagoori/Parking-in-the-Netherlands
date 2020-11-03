3d0b985b1f19f80c6b853431f19e60ae1deed9ec
sjagoori <s.jagoori@outlook.com>
2020-11-03 13:09:15 +0100

    [Added] dynamic width to barchart

--- modules/charts.js
+++ modules/charts.js
@@ -5,22 +5,22 @@ export function barchart(height, width, id, data) {
   const barOffset = 5
 
   d3.select("#" + id).append("svg")
-    .attr("width", width)
+    .attr("width", (data.length * 40))
     .attr("height", height)
     .style("background-color", "#727272")
     .style('padding', '10px 10px 0 10px')
     .selectAll('rect')
-      .data(data)
-      .enter().append("rect")
-      .style("fill", '#ffcc00')
-      .attr("width", 35)
-      .attr("height", (d) => {
-        return d
-      })
-      .attr("x", (d, i) => {
-        return i * (barWidth + barOffset)
-      })
-      .attr("y", (d) => {
-        return height - d
-      })
+    .data(data)
+    .enter().append("rect")
+    .style("fill", '#ffcc00')
+    .attr("width", 35)
+    .attr("height", (d) => {
+      return d
+    })
+    .attr("x", (d, i) => {
+      return i * (barWidth + barOffset)
+    })
+    .attr("y", (d) => {
+      return height - d
+    })
 }
