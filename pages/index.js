41d62d6a8583961171c07f0549866f3f2158a0cb
sjagoori <s.jagoori@outlook.com>
2020-11-21 21:19:27 +0100

    formatting

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,16 @@ export default class Index extends React.Component {
       <>
         <GlobalStyle />
         <ChartContainer>
-          <Map 
-          id="mapchart" 
-          primarySet='https://opendata.rdw.nl/resource/b3us-f26s.json'
-          secondarySet='https://opendata.rdw.nl/resource/t5pc-eb34.json'
-          mapData='https://cartomap.github.io/nl/wgs84/gemeente_2020.topojson'
-          filterOptions={['disabled', 'regular']}
+
+          <Map
+            id='mapchart'
+            title='Dispersion of parkingspaces per category'
+            primarySet='https://opendata.rdw.nl/resource/b3us-f26s.json'
+            secondarySet='https://opendata.rdw.nl/resource/t5pc-eb34.json'
+            mapData='https://cartomap.github.io/nl/wgs84/gemeente_2020.topojson'
+            filterOptions={['disabled', 'regular']}
           />
+
         </ChartContainer>
       </>
     )
