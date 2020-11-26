0cac769c3423c93eec3d537f87872a0ba75899f2
sjagoori <s.jagoori@outlook.com>
2020-11-26 23:14:02 +0100

    Added copy and credits

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,18 @@ export default class Index extends React.Component {
           <GlobalStyle />
 
           <ChartContainer>
+            <SlideText>
+              <h1>Have you ever wondered how disabled-friendly the Dutch parkingspaces are?</h1>
+            </SlideText>
+          </ChartContainer>
+
+          <Devider />
+
+          <ChartContainer>
             <Pie
               id='pieChart'
-              title='Lorem ipsum'
-              lead='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse'
+              title='The ratio of disabled-friendly parkingspaces'
+              lead='In this research, we have examined 1000 parkingspaces. Out of the 1000, only 55 have parkingspots available that are suitable for disabled drivers.'
               primarySet='https://opendata.rdw.nl/resource/b3us-f26s.json'
             />
           </ChartContainer>
@@ -22,10 +30,19 @@ export default class Index extends React.Component {
           <Devider />
 
           <ChartContainer>
+            <SlideText>
+              <h1>Shocking right?</h1>
+              <p>It maybe is, but it gets worse</p>
+            </SlideText>
+          </ChartContainer>
+
+          <Devider />
+
+          <ChartContainer>
             <Map
               id='disabledMap'
-              title='Disabled'
-              lead='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse'
+              title='Dispersion of disabled-friendly parking garages'
+              lead='Here is where is gets nasty; of the 355 parking garages we examined, only 6 of are disabled-friendly. That means that there is at least an exclusive parkingspot in the garage for disabled drivers. Although it is just 6, they at least spread them evenly over the most populated provinces'
               primarySet='https://opendata.rdw.nl/resource/b3us-f26s.json'
               secondarySet='https://opendata.rdw.nl/resource/t5pc-eb34.json'
               mapData='https://cartomap.github.io/nl/wgs84/gemeente_2020.topojson'
@@ -37,8 +54,8 @@ export default class Index extends React.Component {
           <ChartContainer>
             <Map
               id='allMap'
-              title='All'
-              lead='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse'
+              title='Dispersion of regular parking garages'
+              lead='There are 355 parking garages in this map; as you can see, they are usually dispersed about evenly with the exception high populated areas like the Randstad.'
               secondarySet='https://opendata.rdw.nl/resource/t5pc-eb34.json'
               mapData='https://cartomap.github.io/nl/wgs84/gemeente_2020.topojson'
             />
@@ -49,14 +66,24 @@ export default class Index extends React.Component {
           <ChartContainer>
             <Map
               id='completeMap'
-              title='Complete'
-              lead='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse'
+              title='Dispersion of parkingspaces per category'
+              lead='If you toggle between the categories, you will see that only a fraction of parking garages are actually disabled friendly.'
               primarySet='https://opendata.rdw.nl/resource/b3us-f26s.json'
               secondarySet='https://opendata.rdw.nl/resource/t5pc-eb34.json'
               mapData='https://cartomap.github.io/nl/wgs84/gemeente_2020.topojson'
               filterOptions={['disabled', 'regular']}
             />
           </ChartContainer>
+
+          <Devider />
+
+          <ChartContainer>
+            <SlideText>
+              <i>Data by <a href="https://opendata.rdw.nl/">RDW</a> </i>
+              <i>Development by  <a href="https://www.github.com/sjagoori">sjagoori</a> </i>
+            </SlideText>
+          </ChartContainer>
+
         </FullPage>
       </>
     )
@@ -90,6 +117,18 @@ const ChartContainer = styled.div`
   align-items: center; 
   justify-content: center;
   flex-shrink: 0;
+`
+const SlideText = styled.div`
+  max-width: 900px;
+  text-align: center;
+
+  & i { 
+    display: block;
+    margin: 5px;
+    a {
+      color: #FF333D
+    }
+  }
 `
 
 const Devider = styled.hr`
